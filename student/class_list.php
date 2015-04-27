e0ec43964105ef3be29186a612a257e515611e56
Lucas Chaufournier <lucasch@gwu.edu>
2015-04-26 20:45:30 -0400

    updated linking

--- student/class_list.php
+++ student/class_list.php
@@ -1,9 +1,9 @@
-<?php 
+<?php
     session_start();
     $_SESSION['course_id'] = 123;
     $_SESSION['studentName'] = "Lucas Chaufournier";
     $_SESSION['studentEmail'] = "lucasch@gwu.edu";
-    $_SESSION['SID'] = 1;
+    $_SESSION['SID'] = 1234;
     $studentName = $_SESSION['studentName'];
     $studentEmail = $_SESSION['studentEmail'];
     $SID = $_SESSION['SID'];
@@ -13,7 +13,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet"  href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>   
+<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
 <title>Class Roster</title>
 
 </head>
@@ -24,11 +24,11 @@
 <div class="container">
 <center><ul class="pagination">
 <li class="active"><a href="#">Students</a></li>
-<li><a href="features.php">Features</a></li> 
+<li><a href="features.php">Features</a></li>
 </ul></center>
     <div class="jumbotron">
         <?php
-    echo "<b>Name:</b> $studentName"; 
+    echo "<b>Name:</b> $studentName";
     echo "<br>";
     echo "<b>Email:</b> $studentEmail";
     ?>
@@ -44,11 +44,11 @@
         <th>Status</th>
         <th>Action</th>
         <th>Enter</th>
-        
+
     </tr>
     </thead>
     <tbody>
-    
+
 <?php
 include("database.php");
 
@@ -61,7 +61,7 @@ $dbc = mysql_connect($dbServer,$dbUser,$dbPass)
 mysql_select_db($dbName,$dbc);
 
 $query = "select * from (select profile.ID, profile.SID, profile.dir,profile.dockinst,classes.classid,classes.classname,classes.instructor from profile Inner join classes on profile.CID = classes.classid) as A where A.SID=$SID";
-$res = mysql_query($query) or 
+$res = mysql_query($query) or
     die('Error querying database. Query is '.$query);
 
 while($column = mysql_fetch_array($res)){
@@ -72,13 +72,13 @@ while($column = mysql_fetch_array($res)){
     echo "<td>".$column['dir']."</td>";
     echo "<td></td>";
     echo "<td></td>";
-    echo "<td> <form action='student_view.php' method='post'><input type='hidden' value='".$column['SID']."' name='student_id'><input button type='submit' class='btn btn-info' name='username' value='View'></button></form></td>";
+    echo "<td> <form action='/".$column['port']."' method='post'><input type='hidden' value='".$column['SID']."' name='student_id'><input button type='submit' class='btn btn-info' name='username'  value='View'></button></form></td>";
     echo "</tr>";
 }
 
 
 ?>
-        
+
     </tbody>
     </table>
     </div>
